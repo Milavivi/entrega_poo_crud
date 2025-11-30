2f15e20893e33a72f203b90e7a704eeedc7b0c80
vivi <camila.silva314@aluno.ce.gov.br>
2025-11-30 20:12:18 -0300

    atualizações nas dependências(breeze e tailwind)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+<<<<<<< HEAD
+=======
 import tailwindcss from '@tailwindcss/vite';
+>>>>>>> 3b157a2a34b724fd8ecadfdccc93d3fcf52b3b4e
 
 export default defineConfig({
     plugins: [
@@ -8,6 +11,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+<<<<<<< HEAD
+=======
         tailwindcss(),
+>>>>>>> 3b157a2a34b724fd8ecadfdccc93d3fcf52b3b4e
     ],
 });

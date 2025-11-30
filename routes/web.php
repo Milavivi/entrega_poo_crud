1e85c1317261a82c53cd9ab090e15d15c6015e38
vivi <camila.silva314@aluno.ce.gov.br>
2025-11-30 20:14:40 -0300

    Rotas atualizadas com a presença do breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,37 @@
 <?php
 
+<<<<<<< HEAD
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ReceitaController;
+use App\Http\Controllers\IngredienteController;
+use App\Http\Controllers\AvaliacoesController;
+use App\Http\Controllers\FavoritoController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('categorias', CategoriaController::class);
+    Route::resource('receitas', ReceitaController::class);
+    Route::resource('ingredientes', IngredienteController::class);
+    Route::resource('avaliacoes', AvaliacoesController::class);
+    Route::resource('favoritos', FavoritoController::class);
+});
+
+require __DIR__.'/auth.php';
+=======
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
 // Route::get('/', function () {
@@ -15,3 +47,4 @@ Route::get('/categorias/{categoria}/edit',[CategoriaController::class, "edit"])-
 Route::put('/categorias/{categoria}',[CategoriaController::class, "update"])->name("categorias.update");
 
 Route::delete('/categorias/{categoria}',[CategoriaController::class, "destroy"])->name("categorias.destroy");
+>>>>>>> 3b157a2a34b724fd8ecadfdccc93d3fcf52b3b4e
